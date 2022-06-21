e67bf97b98a9ee9b69b94acb8deebb7c8442feb5
Mariana Santos <85956510+marianasantos3@users.noreply.github.com>
2022-06-21 20:15:39 -0300

    Add files via upload
    Testando os aprendizado nas aulas em prática!!!

---
+++ Atividades-proa/Html Css Java Script/Atividade JavaScript/show-hide.js
@@ -0,0 +1,30 @@
+function show_sol()
+{
+    document.getElementById("sol").style.visibility="visible";
+    document.getElementById("floress").style.visibility="hidden";
+    document.getElementById("porDoSol").style.visibility="hidden";
+
+}
+
+function show_floress()
+{
+    document.getElementById("sol").style.visibility="hidden";
+    document.getElementById("floress").style.visibility="visible";
+    document.getElementById("porDoSol").style.visibility="hidden";
+
+}
+
+function show_porDoSol()
+{
+    document.getElementById("sol").style.visibility="hidden";
+    document.getElementById("floress").style.visibility="hidden";
+    document.getElementById("porDoSol").style.visibility="visible";
+
+}
+
+function some_tudo()
+{
+    document.getElementById("sol").style.visibility="hidden";
+    document.getElementById("floress").style.visibility="hidden";
+    document.getElementById("porDoSol").style.visibility="hidden";
+}
